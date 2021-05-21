3b34bf3340d519a117cd1bcb263d82c4bfb54690
ArkaitzUlibarri <arkaitz.ulibarri@hotmail.com>
2021-05-21 21:59:51 +0200

    Refactor

--- apalabrados.py
+++ apalabrados.py
@@ -4,103 +4,124 @@ import os
 from tile import Tile
 import yaml
 
-with open("config/config.yml", 'r') as ymlfile:
-    cfg = yaml.load(ymlfile)
+with open("config/config.yml", 'r') as yml_file:
+    config = yaml.load(yml_file)
 
-BONUS = cfg['bonus']
-APP_NAME = cfg['appname']
-FONT_SIZE = cfg['font']['size']
-DEFAULT_FONT = cfg['font']['type']
-FONT_BOLD = cfg['font']['bold']
+BONUS = config['bonus']
+APP_NAME = config['app_name']
+FONT_SIZE = config['font']['size']
+DEFAULT_FONT = config['font']['type']
+FONT_BOLD = config['font']['bold']
+GRID_COLOR = BLACK = (0, 0, 0)
 
-USER_FICHAS = 7
+USER_TOKENS = 7
 SIZE = WIDTH, HEIGHT = 600, 600
 MAP_SIZE = 15
-SQUARE_SIZE = (WIDTH/MAP_SIZE)
-MEDIUM = SQUARE_SIZE/2
+SQUARE_SIZE = (WIDTH / MAP_SIZE)
+MEDIUM = SQUARE_SIZE / 2
 
-Types = ["Normal","DL","DP","TL","TP","Start","Letter"]
-Letters  = ["A","B","C","D","E","F","G","H","I","J","L","M","N","Ñ","O","P","Q","R","S","T","U","V","X","Y","Z","*"]
-Values = [1,3,3,2,1,4,2,4,1,8,1,3,1,8,1,3,5,1,1,1,1,4,8,4,10,0]
-Tiles = [12,2,4,5,12,1,2,2,6,1,4,2,5,1,9,2,1,5,6,4,5,1,1,1,1,2]
+Types = ["Normal", "DL", "DP", "TL", "TP", "Start", "Letter"]
+Letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "L", "M", "N", "Ñ", "O", "P", "Q",
+           "R", "S", "T", "U", "V", "X", "Y", "Z", "*"]
+Values = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 1, 3, 1, 8, 1, 3, 5, 1, 1, 1, 1, 4, 8, 4, 10, 0]
+Tiles = [12, 2, 4, 5, 12, 1, 2, 2, 6, 1, 4, 2, 5, 1, 9, 2, 1, 5, 6, 4, 5, 1, 1, 1, 1, 2]
 
 Quantity = len(Letters)
 totalTiles = sum(Tiles)
 
-letters = {'A': [1,12], 'B': [3,2], 'C': [3,4], 'D': [2,5], 'E': [1,12], 'F': [4,1], 'G': [2,2], 'H': [4,2], 'I': [1,6], 'J': [8,1], 'L': [1,4], 'M': [3,2], 
-'N': [1,5], 'Ñ': [8,1], 'O':[1,9], 'P': [3,2], 'Q': [5,1], 'R': [1,5], 'S': [1,6], 'T': [1,4], 'U': [1,5], 'V': [4,1], 'X': [8,1], 'Y': [4,1], 'Z': [10,1], 
-'*': [0,2] } 
+letters = {'A': [1, 12], 'B': [3, 2], 'C': [3, 4], 'D': [2, 5], 'E': [1, 12], 'F': [4, 1], 'G': [2, 2], 'H': [4, 2],
+           'I': [1, 6], 'J': [8, 1], 'L': [1, 4], 'M': [3, 2], 'N': [1, 5], 'Ñ': [8, 1], 'O': [1, 9], 'P': [3, 2],
+           'Q': [5, 1], 'R': [1, 5], 'S': [1, 6], 'T': [1, 4], 'U': [1, 5], 'V': [4, 1], 'X': [8, 1], 'Y': [4, 1],
+           'Z': [10, 1], '*': [0, 2]}
 
-def multiplyLetter(squareType,letterValue):
-	if(squareType == "DL"):
-		return 2 * letterValue
-	elif(squareType == "TL"):
-		return 3 * letterValue
-	else:
-		return letterValue
 
-def multiplyWord(squareType,wordValue):
-	if(squareType == "DP"):
-		return 2 * wordValue
-	elif(squareType == "TP"):
-		return 3 * wordValue
-	else:
-		return wordValue
+def multiply_letter(square_type, letter_value):
+    if square_type == "DL":
+        return 2 * letter_value
+    elif square_type == "TL":
+        return 3 * letter_value
+    else:
+        return letter_value
 
-def applyBonus(wordValue):
-	return wordValue + BONUS
 
-def mapeoType(number):
-	return Types[number]
+def multiply_word(square_type, word_value):
+    if square_type == "DP":
+        return 2 * word_value
+    elif square_type == "TP":
+        return 3 * word_value
+    else:
+        return word_value
 
-def loadMap(filename = "assets/map"):
-	if (not os.path.isfile(filename)):
-		return False
 
-	f = open(filename, "r")
-	data = f.read().split("\n")
+def apply_bonus(word_value):
+    return word_value + BONUS
 
-	matrix = []
-	for index,row in enumerate(data):
-		matrix.append([row])
 
-	return matrix
+def type_mapping(number):
+    return Types[number]
+
+
+def load_map(filename="assets/map"):
+    if not os.path.isfile(filename):
+        return False
+
+    f = open(filename, "r")
+    data = f.read().split("\n")
+
+    matrix = []
+    for index, row in enumerate(data):
+        matrix.append([row])
+
+    return matrix
+
+
+def draw_grid(surface):
+    square_size = WIDTH // MAP_SIZE
+    x = 0
+    y = 0
+    for element in range(MAP_SIZE):
+        x = x + square_size
+        y = y + square_size
+
+    pygame.draw.line(surface, GRID_COLOR, (x, 0), (x, WIDTH))
+    pygame.draw.line(surface, GRID_COLOR, (0, y), (WIDTH, y))
+
 
 def main():
-	pygame.init()
+    pygame.init()
 
-	font = pygame.font.SysFont(DEFAULT_FONT, FONT_SIZE, FONT_BOLD)
+    font = pygame.font.SysFont(DEFAULT_FONT, FONT_SIZE, FONT_BOLD)
+    screen = pygame.display.set_mode(SIZE)  # Create canvas to draw the objects of the game
+    pygame.display.set_caption(APP_NAME)  # Change window title
 
-	screen = pygame.display.set_mode(SIZE)  # crear el canvas dónde vamos a dibujar los diferentes objetos del videojuego
-	pygame.display.set_caption(APP_NAME)  # cambiar el título de la ventana
+    main_map = load_map()
 
-	mapr = loadMap()
-	
-	squares = []
-	for rowIndex,rowValue in enumerate(mapr):
-		rowValue = rowValue[0].split(',')
-		for colIndex,colValue in enumerate(rowValue):
-			squareType = mapeoType(int(colValue))
-			point = (MEDIUM + int(rowIndex) * SQUARE_SIZE,MEDIUM + int(colIndex) * SQUARE_SIZE)
-			newTile = Tile(squareType,font,point)
-			squares.append(newTile)
+    squares = []
+    for rowIndex, row_value in enumerate(main_map):
+        row_value = row_value[0].split(',')
+        for colIndex, colValue in enumerate(row_value):
+            square_type = type_mapping(int(colValue))
+            point = (MEDIUM + int(rowIndex) * SQUARE_SIZE, MEDIUM + int(colIndex) * SQUARE_SIZE)
+            new_tile = Tile(square_type, font, point)
+            squares.append(new_tile)
 
-	sprites = pygame.sprite.Group(squares)
+    sprites = pygame.sprite.Group(squares)
 
-	while 1:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				return
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
 
-		sprites.update() #actualice y dibuje en pantalla todos los sprites
-		# screen.blit(background, (0, 0)) #copia los pixeles contenidos en la imagen de fondo sobre el canvas
-		sprites.draw(screen)
-		pygame.display.flip() #hacer un cambio de buffers
+        sprites.update()  # Update and draw in screen all the sprites
+        sprites.draw(screen)
+        draw_grid(screen)
+        pygame.display.update()
 
-# Llamamos al main cuando se invoca el archivo como un programa
+
+# We call main when the file is invoked as a program
 if __name__ == '__main__':
-	os.system("cls")
-	print ("Game Start: "+ datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-	main()
-	print ("Game End: "+ datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    os.system("cls")
+    print("Game Start: " + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    main()
+    print("Game End: " + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
--- tile.py
+++ tile.py
@@ -1,45 +1,47 @@
 import pygame
 
+
 class Tile(pygame.sprite.Sprite):
-	SIZE = (WIDTH, HEIGHT) = (40,40) # 40x40
+    SIZE = (WIDTH, HEIGHT) = (40, 40)  # 40x40
 
-	dic_colors = {'yellow': (255,239,25), 'white': (255,255,255), 'red': (255,0,0), 'green': (0,255,0), 
-	'blue': (0,0,255), 'orange': (239,127,26), 'black': (0,0,0)}
+    dic_colors = {'yellow': (255, 239, 25), 'white': (255, 255, 255), 'red': (255, 0, 0), 'green': (0, 255, 0),
+                  'blue': (0, 0, 255), 'orange': (239, 127, 26), 'black': (0, 0, 0)}
 
-	def __init__(self,squareType,font,pos=(0,0)):
-		pygame.sprite.Sprite.__init__(self)
-		self.pos = pos
-		self.squareType = squareType
-		self.text = self.setText()
-		self.color = self.setColor()
-		self.font = font
-		self.image = pygame.Surface(Tile.SIZE).convert()
-		self.image.fill(self.color)
-		label = self.font.render(str(self.text), 0, self.dic_colors['white'])
-		coords = (Tile.WIDTH / 2. - (label.get_rect().width / 2.), Tile.HEIGHT / 2. - (label.get_rect().height / 2.))
-		self.image.blit(label,coords)
-		self.rect = self.image.get_rect(center=self.pos)
-		#self.value = 0
-		#self.quantity = 0
+    def __init__(self, square_type, font, pos=(0, 0)):
+        pygame.sprite.Sprite.__init__(self)
+        self.pos = pos
+        self.square_type = square_type
+        self.text = self.set_text()
+        self.color = self.set_color()
+        self.font = font
+        self.image = pygame.Surface(Tile.SIZE).convert()
+        self.image.fill(self.color)
+        label = self.font.render(str(self.text), 0, self.dic_colors['white'])
+        coords = (Tile.WIDTH / 2. - (label.get_rect().width / 2.), Tile.HEIGHT / 2. - (label.get_rect().height / 2.))
+        self.image.blit(label, coords)
+        self.rect = self.image.get_rect(center=self.pos)
 
-	def setColor(self):
-		if(self.squareType == "Normal"):
-			return self.dic_colors['white']
-		elif(self.squareType == "DL"):
-			return self.dic_colors['blue'] 
-		elif(self.squareType == "DP"):
-			return self.dic_colors['orange']  
-		elif(self.squareType == "TL"):
-			return self.dic_colors['green']  
-		elif(self.squareType == "TP"):
-			 return self.dic_colors['red']  
-		elif(self.squareType == "Start"):
-			return self.dic_colors['black']   
-		elif(self.squareType == "Letter"):
-			return self.dic_colors['yellow']
+    #  self.value = 0
+    #  self.quantity = 0
 
-	def setText(self):
-		if(self.squareType == "Normal" or self.squareType == "Start" or self.squareType == "Letter"):
-			return ""
-		else:
-			return self.squareType
+    def set_color(self):
+        if self.square_type == "Normal":
+            return self.dic_colors['white']
+        elif self.square_type == "DL":
+            return self.dic_colors['blue']
+        elif self.square_type == "DP":
+            return self.dic_colors['orange']
+        elif self.square_type == "TL":
+            return self.dic_colors['green']
+        elif self.square_type == "TP":
+            return self.dic_colors['red']
+        elif self.square_type == "Start":
+            return self.dic_colors['black']
+        elif self.square_type == "Letter":
+            return self.dic_colors['yellow']
+
+    def set_text(self):
+        if self.square_type == "Normal" or self.square_type == "Start" or self.square_type == "Letter":
+            return ""
+        else:
+            return self.square_type
